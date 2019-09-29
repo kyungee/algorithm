aa2b3eec0e63cdb7a154766c2649d3bb3f347c8e
kyungee <kyungee.kim@gmail.com>
2019-09-29 16:17:56 +0900

    N과 M (5)

---
+++ acmicpc/acm_15654.cpp
@@ -0,0 +1,46 @@
+
+// https://www.acmicpc.net/problem/15654
+
+#include <vector>
+#include <algorithm>
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int n, m;
+int arr[10];
+bool visit[10];
+vector<int> v;
+
+void DFS(int cnt) {
+    if(cnt == m) {
+        for(int i=0; i<v.size(); i++) {
+            printf("%d ", v[i]);
+        }
+        printf("\n");
+        return;
+    }
+    
+    for(int i=0; i<n; i++) {
+        if(visit[i] == true) continue;
+        visit[i] = true;
+        v.push_back(arr[i]);
+        DFS(cnt+1);
+        v.pop_back();
+        visit[i] = false;
+    }
+    
+}
+
+int main() {
+    scanf("%d %d", &n, &m);
+    
+    for(int i=0; i<n; i++) {
+        scanf("%d ", &arr[i]);
+    }
+    
+    sort(arr, arr+n);
+    DFS(0);
+    
+    return 0;
+}
